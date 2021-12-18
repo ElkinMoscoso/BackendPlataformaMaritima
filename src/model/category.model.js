e64aee6bb131d2475472bcf5940a4652ab5bb98a
ElkinMoscoso <elkinmoscoso@uninorte.edu.co>
2021-12-18 10:21:27 -0500

    elimina prod,cat,lot

--- src/model/category.model.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const categorySchema = new Schema({
-  name: { type: String, required: true },
-  state: { type: Boolean, default: true },
-});
-
-export default model('Category', categorySchema);
--- src/model/lot.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const lotSchema = new Schema({
-  name: String,
-  date: Date,
-  state: { type: Boolean, default: true },
-});
-
-export default model('Lot', lotSchema);
